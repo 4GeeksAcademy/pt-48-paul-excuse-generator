7f0751729c23de3894142ff87562b66d5adf1b4a
Paul <140537985+Paulmak21@users.noreply.github.com>
2023-10-03 18:20:00 +0000

    added index.html && index.js and wrote the code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excuseGenerator(array) {
+  let theExcuse;
+  theExcuse = array[Math.floor(Math.random() * array.length)];
+  return theExcuse;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function newExcuse(who, action, what, when) {
+  let myNewExcuse = "";
+  myNewExcuse =
+    excuseGenerator(who) +
+    " " +
+    excuseGenerator(action) +
+    " " +
+    excuseGenerator(what) +
+    " " +
+    excuseGenerator(when) +
+    ".";
+  return myNewExcuse;
+}
+
+let generateExcuse = newExcuse(who, action, what, when);
+
+document.getElementById("excuse").innerHTML = generateExcuse;
